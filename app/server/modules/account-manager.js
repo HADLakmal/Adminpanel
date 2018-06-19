772788f21fe3d7e62ebed0b4c28aa415f0834a67
Damidnu Lakmal <damindu.14@cse.mrt.ac.lk>
2018-06-19 09:00:43 +0530

    change userupdate with withdraw

--- app/server/modules/account-manager.js
+++ app/server/modules/account-manager.js
@@ -255,7 +255,7 @@ exports.addNewUser =  function(newData, callback)
 
                 }
                 else{
-					var newvalues = {$set: {id: newData.id,name:newData.name}};
+					var newvalues = {$set: {id: newData.id,name:newData.name,withdraw : false,freeze : false}};
 					users.updateOne(myquery, newvalues, function (e, result) {
 						if (e) callback(e)
 						else {
